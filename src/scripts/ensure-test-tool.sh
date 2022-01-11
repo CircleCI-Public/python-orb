3ddfa95cd7a148440dff891ffed8c4ca07a3703b
Jaryt Bustard <jarytbustard@gmail.com>
2022-01-10 21:50:05 -0500

    feat: ensure test tool is installed

---
+++ src/scripts/ensure-test-tool.sh
@@ -0,0 +1,37 @@
+PYTHON_ORB_PKG_MNGR="pipenv"
+
+PYTHON_ORB_TEST_TOOL="pytest"
+
+case $PYTHON_ORB_PKG_MNGR in
+    pip)
+        REQUIREMENTS_PATH="requirements.txt"
+    ;;
+    pipenv) # TODO: use PIPENV_PIPFILE
+        REQUIREMENTS_PATH="Pipfile"
+        PYTHON_ENV_TOOL="pipenv"
+    ;;
+    poetry)
+        REQUIREMENTS_PATH="pyproject.toml"
+        PYTHON_ENV_TOOL="poetry"
+    ;;
+esac
+
+if [ -f ${REQUIREMENTS_PATH} ]; then
+    echo "INFO: Detected dependency file: $REQUIREMENTS_PATH"
+else
+    echo "WARNING: No dependency file for ${PYTHON_ORB_PKG_MNGR} found. ${REQUIREMENTS_PATH} expected."
+fi
+
+# Automatically install test package. unittest is preinstalled and not required.
+if [ "${PYTHON_ORB_TEST_TOOL}" != "unittest" ]; then
+    DETECT_TEST_TOOL=$(eval "${PYTHON_ENV_TOOL:+$PYTHON_ENV_TOOL run} pip --disable-pip-version-check list" |
+    awk 'NR > 2 { print $1 }' | grep "^${PYTHON_ORB_TEST_TOOL}$")
+    
+    # If the test package is not detected, install using PYTHON_INSTALL_TOOL
+    if [ -z "$DETECT_TEST_TOOL" ]; then
+        echo "INFO: Test package ${PYTHON_ORB_TEST_TOOL} was not found. Installing..."
+        eval "${PYTHON_ENV_TOOL:-pip} install ${PYTHON_ORB_TEST_TOOL}"
+    else
+        echo "INFO: Detected test package: $DETECT_TEST_TOOL"
+    fi
+fi
