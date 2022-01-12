3ec694d5695a64f1c56b7a26148454303940545c
Jaryt Bustard <jarytbustard@gmail.com>
2022-01-12 14:13:23 -0500

    chore: evaluate error code

--- src/scripts/ensure-test-tool.sh
+++ src/scripts/ensure-test-tool.sh
@@ -1,4 +1,4 @@
-case $PYTHON_ORB_PKG_MNGR in
+case $PARAM_PKG_MNGR in
     pip)
         REQUIREMENTS_PATH="requirements.txt"
     ;;
@@ -12,24 +12,24 @@ case $PYTHON_ORB_PKG_MNGR in
     ;;
 esac
 
+set -x
+
 if [ -f ${REQUIREMENTS_PATH} ]; then
     echo "INFO: Detected dependency file: $REQUIREMENTS_PATH"
 else
-    echo "WARNING: No dependency file for ${PYTHON_ORB_PKG_MNGR} found. ${REQUIREMENTS_PATH} expected."
+    echo "WARNING: No dependency file for ${PARAM_PKG_MNGR} found. ${REQUIREMENTS_PATH} expected."
 fi
 
 # Automatically install test package. unittest is preinstalled and not required.
-if [ "${PYTHON_ORB_TEST_TOOL}" != "unittest" ]; then
+if [ "${PARAM_TEST_TOOL}" != "unittest" ]; then
     DETECT_TEST_TOOL=$(eval "${PYTHON_ENV_TOOL:+$PYTHON_ENV_TOOL run} pip --disable-pip-version-check list" |
-    awk 'NR > 2 { print $1 }' | grep "^${PYTHON_ORB_TEST_TOOL}$")
+    awk 'NR > 2 { print $1 }' | grep "^${PARAM_TEST_TOOL}$")
     
     # If the test package is not detected, install using PYTHON_INSTALL_TOOL
     if [ -z "$DETECT_TEST_TOOL" ]; then
-        echo "INFO: Test package ${PYTHON_ORB_TEST_TOOL} was not found. Installing..."
-        eval "${PYTHON_ENV_TOOL:-pip} install ${PYTHON_ORB_TEST_TOOL}"
+        echo "INFO: Test package ${PARAM_TEST_TOOL} was not found. Installing..."
+        eval "${PYTHON_ENV_TOOL:-pip} install ${PARAM_TEST_TOOL}"
     else
         echo "INFO: Detected test package: $DETECT_TEST_TOOL"
     fi
 fi
-
-exit 0 # should always pass
