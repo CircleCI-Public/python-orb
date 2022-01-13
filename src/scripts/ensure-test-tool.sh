d49483e6e2e917e6aefe515bf3b3ed36f2e03ace
Jaryt Bustard <jarytbustard@gmail.com>
2022-01-13 14:46:53 -0500

    feat: added pip-dist support to ensure-test-tool

--- src/scripts/ensure-test-tool.sh
+++ src/scripts/ensure-test-tool.sh
@@ -2,6 +2,10 @@ case $PARAM_PKG_MNGR in
     pip)
         REQUIREMENTS_PATH="requirements.txt"
     ;;
+    pip-dist)
+        PYTHON_INSTALL_ARGS="-e"
+        REQUIREMENTS_PATH="requirements.txt"
+    ;;
     pipenv) # TODO: use PIPENV_PIPFILE
         REQUIREMENTS_PATH="Pipfile"
         PYTHON_ENV_TOOL="pipenv"
@@ -11,9 +15,6 @@ case $PARAM_PKG_MNGR in
         PYTHON_ENV_TOOL="poetry"
     ;;
 esac
-
-set -e
-set -x
 
 if [ -f ${REQUIREMENTS_PATH} ]; then
     echo "INFO: Detected dependency file: $REQUIREMENTS_PATH"
@@ -26,11 +27,22 @@ if [ "${PARAM_TEST_TOOL}" != "unittest" ]; then
     DETECT_TEST_TOOL=$(eval "${PYTHON_ENV_TOOL:+$PYTHON_ENV_TOOL run} pip --disable-pip-version-check list" |
     awk 'NR > 2 { print $1 }' | grep "^${PARAM_TEST_TOOL}$")
     
+    NOT_DETECTED=$?
+    if [ $NOT_DETECTED -gt 0 ] && [ "${PARAM_FAIL_IF_MISSING_TOOL}" -eq 0 ]; then
+        exit 0
+    fi
+    
     # If the test package is not detected, install using PYTHON_INSTALL_TOOL
     if [ -z "$DETECT_TEST_TOOL" ]; then
         echo "INFO: Test package ${PARAM_TEST_TOOL} was not found. Installing..."
-        eval "${PYTHON_ENV_TOOL:-pip} install ${PARAM_TEST_TOOL}"
+        eval "${PYTHON_ENV_TOOL:-pip} install ${PYTHON_INSTALL_ARGS} ${PARAM_TEST_TOOL}"
+        INSTALL_RESULT=$?
     else
         echo "INFO: Detected test package: $DETECT_TEST_TOOL"
+    fi
+    
+    # If the test package is not detected and PARAM_FAIL_IF_MISSING_TOOL is 0, succeed anyways
+    if [ $NOT_DETECTED -gt 0 ] && [ "${PARAM_FAIL_IF_MISSING_TOOL}" -eq 0 ]; then
+        exit ${INSTALL_RESULT:-0}
     fi
 fi
