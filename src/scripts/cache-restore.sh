7887c62dc85bb8e9291a023f2fb1ea2a4b696935
Jaryt Bustard <jarytbustard@gmail.com>
2022-01-20 12:47:06 -0500

    fix: fixed restore_paths func

--- src/scripts/cache-restore.sh
+++ src/scripts/cache-restore.sh
@@ -1,9 +1,11 @@
 restore_paths() {
-    for file in "${1}"/*; do
-        echo "INFO: Restoring ${file}"
-        decoded=$(basename "${file}" | base64 -d)
-        mv file "${decoded}"
-    done
+    if [ -d "${1}" ]; then
+        for file in "${1}"/*; do
+            decoded=$(basename "${file}" | base64 -d)
+            mv "${file}" "${decoded}"
+            echo "INFO: Restoring ${file} to ${decoded}"
+        done
+    fi
 }
 
 if [ "${PARAM_VENV_CACHE}" = "1" ]; then
@@ -13,3 +15,11 @@ fi
 if [ "${PARAM_PYPI_CACHE}" = "1" ]; then
     restore_paths "/tmp/pypi_cache"
 fi
+
+restore_paths() {
+    for file in "${1}"/.*; do
+        echo "INFO: Restoring ${file}"
+        decoded=$(basename "${file}" | base64 -d)
+        mv file "${decoded}"
+    done
+}
