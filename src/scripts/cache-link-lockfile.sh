3138c0debd7de1e42780e554db37b9c93e2856fb
Jaryt Bustard <jarytbustard@gmail.com>
2022-01-20 11:40:44 -0500

    fix: added readlink to expand home path

--- src/scripts/cache-link-lockfile.sh
+++ src/scripts/cache-link-lockfile.sh
@@ -4,24 +4,24 @@ source "$AUTO_DETECT_ENV_SCRIPT"
 if [ ! -f "/tmp/lockfile" ]; then
     case ${DETECT_PKG_MNGR:-${PARAM_PKG_MNGR}} in
         pip | pip-dist)
-            LOCK_FILE="${PARAM_APP_DIR}/${PARAM_DEPENDENCY_FILE:-requirements.txt}"
+            LOCK_FILE=$(readlink -f "${PARAM_APP_DIR}"/"${PARAM_DEPENDENCY_FILE:-requirements.txt}")
         ;;
-        pipenv) # TODO: use PIPENV_PIPFILE
-            LOCK_FILE="${PARAM_APP_DIR}/Pipfile.lock"
+        pipenv)
+            LOCK_FILE=$(readlink -f "${PARAM_APP_DIR}"/Pipfile.lock)
         ;;
         poetry)
-            LOCK_FILE="${PARAM_APP_DIR}/poetry.lock"
+            LOCK_FILE=$(readlink -f "${PARAM_APP_DIR}"/poetry.lock)
         ;;
     esac
     
     if [ -z "${LOCK_FILE}" ]; then
-        echo "ERROR: Could not determine lockfile path for ${DETECT_PKG_MNGR:-PARAM_PKG_MNGR}"
+        echo "WARNING: Could not determine lockfile path for ${DETECT_PKG_MNGR:-PARAM_PKG_MNGR}"
     else
       if [ -f "${LOCK_FILE}" ]; then
         echo "INFO: Linking ${LOCK_FILE} to /tmp/lockfile"
         ln "${LOCK_FILE}" "/tmp/lockfile"
       else
-        echo "ERROR: Could not find lockfile at ${LOCK_FILE}"
+        echo "WARNING: Could not find lockfile at ${LOCK_FILE}"
       fi
     fi
 fi
