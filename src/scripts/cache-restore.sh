7d676cc4be9bf63bcf66363b7a4b8935f6549632
Jaryt Bustard <jarytbustard@gmail.com>
2022-01-25 16:31:08 -0500

    chore: remove testing commands

--- src/scripts/cache-restore.sh
+++ src/scripts/cache-restore.sh
@@ -1,6 +1,4 @@
 restore_paths() {
-    test -d "${1}"
-    ls -A "${1}" 2>/dev/null
     if [ -d "${1}" ] && [ -n "$(ls -A "${1}" 2>/dev/null)" ]; then
         for file in "${1}"/*; do
             echo "INFO: Restoring ${file}"
