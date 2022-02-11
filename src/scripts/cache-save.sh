bda11eb1f5010be66e14980e194695d5a50be32c
Jaryt Bustard <jarytbustard@gmail.com>
2022-02-11 15:55:28 -0500

    test: output of readlink

--- src/scripts/cache-save.sh
+++ src/scripts/cache-save.sh
@@ -66,10 +66,10 @@ if [ -f "${LOCKFILE_PATH}" ]; then
     unlink "${LOCKFILE_PATH}"
 fi
 
-echo "${LOCK_FILE} $(readlink -f ${LOCK_FILE})"
+echo "${LOCK_FILE}"
+readlink -f "${LOCK_FILE}"
 
 if [ -e "${LOCK_FILE}" ]; then
-    FULL_LOCK_FILE=$(readlink -f "${LOCK_FILE}")
     
     echo "INFO: Linking ${FULL_LOCK_FILE} to ${LOCKFILE_PATH}"
     ln -s "${FULL_LOCK_FILE}" "${LOCKFILE_PATH}"
