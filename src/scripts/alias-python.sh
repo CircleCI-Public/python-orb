85f0308f0494ef2bdd6db4ab7b15c41a01c14d18
Jaryt <jarytbustard@gmail.com>
2022-09-06 21:02:19 -0400

    chore: remove unsetting expand_aliases

--- src/scripts/alias-python.sh
+++ src/scripts/alias-python.sh
@@ -1,14 +1,8 @@
 if [ ! "${BASH_ENV_PYTHON_ALIASED}" ]; then
     echo 'if [ ! $(command -v python) ]; then
-  EXPAND_ALIASES=$(shopt expand_aliases | cut -d " " -f 2 | xargs)
-
   shopt -s expand_aliases
   alias python=python3
   alias pip=pip3
-
-#   if [ "${EXPAND_ALIASES}" == "off" ]; then
-#     shopt -u expand_aliases
-#   fi
 fi
 
 BASH_ENV_PYTHON_ALIASED=true' >> "$BASH_ENV"
