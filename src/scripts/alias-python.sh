8a2e4c0b6b0d24b1e01abfebcfc5a9d1bb3c9da0
Jaryt <jarytbustard@gmail.com>
2022-09-06 20:58:55 -0400

    test: remove unset

--- src/scripts/alias-python.sh
+++ src/scripts/alias-python.sh
@@ -6,9 +6,9 @@ if [ ! "${BASH_ENV_PYTHON_ALIASED}" ]; then
   alias python=python3
   alias pip=pip3
 
-  if [ "${EXPAND_ALIASES}" == "off" ]; then
-    shopt -u expand_aliases
-  fi
+#   if [ "${EXPAND_ALIASES}" == "off" ]; then
+#     shopt -u expand_aliases
+#   fi
 fi
 
 BASH_ENV_PYTHON_ALIASED=true' >> "$BASH_ENV"
