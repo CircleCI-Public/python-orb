8960f83891a3ec2a190a1f0455764ba7debc6168
Jaryt <jarytbustard@gmail.com>
2022-09-06 20:53:38 -0400

    chore: fix indent

--- src/scripts/alias-python.sh
+++ src/scripts/alias-python.sh
@@ -1,15 +1,15 @@
 if [ ! "${BASH_ENV_PYTHON_ALIASED}" ]; then
     echo 'if [ ! $(command -v python) ]; then
-        EXPAND_ALIASES=$(shopt expand_aliases | cut -d ' ' -f 2 | xargs)
+  EXPAND_ALIASES=$(shopt expand_aliases | cut -d " " -f 2 | xargs)
 
-        shopt -s expand_aliases
-        alias python=python3
-        alias pip=pip3
-        
-        if [ "${EXPAND_ALIASES}" == "off" ];
-            shopt -u expand_aliases
-        fi
-    fi
+  shopt -s expand_aliases
+  alias python=python3
+  alias pip=pip3
 
-    BASH_ENV_PYTHON_ALIASED=true' >> "$BASH_ENV"
+  if [ "${EXPAND_ALIASES}" == "off" ];
+    shopt -u expand_aliases
+  fi
+fi
+
+BASH_ENV_PYTHON_ALIASED=true' >> "$BASH_ENV"
 fi
