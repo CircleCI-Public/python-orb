c5893890c92ab54149a92ef89715426f1694cc4f
Michael A. Smith <michael@smith-li.com>
2022-09-22 17:57:53 -0400

    Use python -m build for PEP517 Compatibility (#102)
    * Use python -m build for PEP517 Compatibility
    
    [build](https://pypa-build.readthedocs.io/en/latest/) is a simple,
    correct PEP 517 build frontend. It invokes the PEP517 hooks, so projects
    that support PEP 517, but not setuptools, will still build.
    
    * Fix Typo
    
    Co-authored-by: Jaryt Bustard <jarytbustard@gmail.com>

---
+++ src/scripts/pypa-build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+main() {
+  local -a build_args
+  build_args=(
+    --outdir "$PARAM_OUTDIR"
+  )
+  [[ $PARAM_SDIST ]] && build_args+=( --sdist )
+  [[ $PARAM_WHEEL ]] && build_args+=( --wheel )
+  [[ $PARAM_SKIP_DEPENDENCY_CHECK ]] && build_args+=( --skip-dependency-check )
+  [[ $PARAM_NO_ISOLATION ]] && build_args+=( --no-isolation )
+  python -m build "${build_args[@]}" .
+  ls -l "$PARAM_OUTDIR"
+}
+
+main "$@"
