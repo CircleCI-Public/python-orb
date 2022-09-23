5e3cf667062e786d038f4b1a52aea85522aca6df
Jaryt <jarytbustard@gmail.com>
2022-09-23 11:22:33 -0400

    fix: argument appending even when false

--- src/scripts/pypa-build.sh
+++ src/scripts/pypa-build.sh
@@ -4,10 +4,10 @@ main() {
   build_args=(
     --outdir "$PARAM_OUTDIR"
   )
-  [[ $PARAM_SDIST == true ]] && build_args+=( --sdist )
-  [[ $PARAM_WHEEL == true ]] && build_args+=( --wheel )
-  [[ $PARAM_SKIP_DEPENDENCY_CHECK == true ]] && build_args+=( --skip-dependency-check )
-  [[ $PARAM_NO_ISOLATION == true ]] && build_args+=( --no-isolation )
+  [[ "$PARAM_SDIST" = true ]] && build_args+=( --sdist )
+  [[ "$PARAM_WHEEL" = true ]] && build_args+=( --wheel )
+  [[ "$PARAM_SKIP_DEPENDENCY_CHECK" = true ]] && build_args+=( --skip-dependency-check )
+  [[ "$PARAM_NO_ISOLATION" = true ]] && build_args+=( --no-isolation )
 
   set -x
   python -m build "${build_args[@]}" .
