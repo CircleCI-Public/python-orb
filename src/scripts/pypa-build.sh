1bbabe8fa87071abfb13879ea97201cb3d8f84d6
Jaryt <jarytbustard@gmail.com>
2022-09-22 20:07:11 -0400

    fix: revert function changes

--- src/scripts/pypa-build.sh
+++ src/scripts/pypa-build.sh
@@ -1,11 +1,16 @@
-local -a build_args
-build_args=(
-  --outdir "$PARAM_OUTDIR"
-)
-[[ $PARAM_SDIST ]] && build_args+=( --sdist )
-[[ $PARAM_WHEEL ]] && build_args+=( --wheel )
-[[ $PARAM_SKIP_DEPENDENCY_CHECK ]] && build_args+=( --skip-dependency-check )
-[[ $PARAM_NO_ISOLATION ]] && build_args+=( --no-isolation )
+#!/usr/bin/env bash
 
-python -m build "${build_args[@]}" .
-ls -l "$PARAM_OUTDIR"
+main() {
+  local -a build_args
+  build_args=(
+    --outdir "$PARAM_OUTDIR"
+  )
+  [[ $PARAM_SDIST ]] && build_args+=( --sdist )
+  [[ $PARAM_WHEEL ]] && build_args+=( --wheel )
+  [[ $PARAM_SKIP_DEPENDENCY_CHECK ]] && build_args+=( --skip-dependency-check )
+  [[ $PARAM_NO_ISOLATION ]] && build_args+=( --no-isolation )
+  python -m build "${build_args[@]}" .
+  ls -l "$PARAM_OUTDIR"
+}
+
+main "$@"
